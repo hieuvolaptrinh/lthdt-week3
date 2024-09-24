e2da72cab4bb499895563f5b0dd312c079154b5c
hieuvolaptrinh <vndhieuak@gmail.com>
2024-09-25 00:44:12 +0700

    commit trc pull

--- java_tuan3_baitap/src/java_tuan3_baitap/Bai1.java
+++ java_tuan3_baitap/src/java_tuan3_baitap/Bai1.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package bai1;
+package java_tuan3_baitap;
 
 import java.util.Scanner;
 
--- java_tuan3_baitap/src/java_tuan3_baitap/bai3.java
+++ java_tuan3_baitap/src/java_tuan3_baitap/bai3.java
@@ -2,8 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package bai1;
-
+package java_tuan3_baitap;
 import java.util.Scanner;
 /**
  *
